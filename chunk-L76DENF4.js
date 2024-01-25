0d61ed8d326de4d832f5b4b890c5c82639418ce7
solzaki <m.m.silhouette@gmail.com>
2024-01-25 13:56:52 +0900

    Auto-generated commit [ci skip]

---
+++ chunk-L76DENF4.js
@@ -0,0 +1,1 @@
+import{Ua as o,Va as a,_ as i,gb as r,mb as p}from"./chunk-E3GFEC76.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-equipment"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,m){n&1&&(o(0,"p"),r(1,"equipment works!"),a())}});let t=e;return t})();export{d as EquipmentComponent};
